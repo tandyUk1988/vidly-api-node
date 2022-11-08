e924f1c6f3770eb7801fb57285f9c6f0a36293f0
tandyUk1988 <tandyuk1988@outlook.com>
2022-11-08 10:19:48 +0000

    added support for connection string via an env variable

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ require("./startup/routes")(app);
 require("./startup/db")();
 require("./startup/config")();
 require("./startup/validation")();
+require("./startup/prod")(app);
 
 module.exports = app;
 
